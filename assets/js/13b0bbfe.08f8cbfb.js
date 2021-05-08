22cf8229b0630a4acf1795a20fc6301716b07458
Sven Kreiss <me@svenkreiss.com>
2021-05-08 14:17:34 +0200

    Deploy website - based on 5312429d9cbb81a2297284b05d4f37d781fb62ca

---
+++ assets/js/13b0bbfe.08f8cbfb.js
@@ -0,0 +1,1 @@
+(self.webpackChunkstructuredai=self.webpackChunkstructuredai||[]).push([[718],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
